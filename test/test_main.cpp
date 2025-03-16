8ce1c12fd60dac27954eb60ef03aead4c0bfe3d2
goldenstain <xym@MSI>
2025-03-16 18:55:30 +0800

    adjust test_main to create the log_dir correctly.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,11 +1,19 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
+#include <filesystem>
 
 int main(int argc, char* argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging("Kuiper");
   FLAGS_log_dir = "./log/";
   FLAGS_alsologtostderr = true;
+  const std::string log_dir = FLAGS_log_dir;
+  if (!std::filesystem::exists(log_dir)) {
+    if (!std::filesystem::create_directories(log_dir)) {
+      std::cerr << "Failed to create the log directory: " << log_dir << std::endl;
+      return -1;
+    }
+  }
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging("Kuiper");
 
   LOG(INFO) << "Start Test...\n";
   return RUN_ALL_TESTS();
